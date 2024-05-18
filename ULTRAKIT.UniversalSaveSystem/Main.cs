b36edd9885b8f116da5bf9ff46f58e2c0b37f55a
PetersonE1 <PetersonE_1@live.com>
2024-05-18 01:20:36 -0400

    Add project files.

---
+++ ULTRAKIT.UniversalSaveSystem/Main.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ULTRAKIT.UniversalSaveSystem
+{
+    [BepInPlugin("ULTRAKIT.UniversalSaveSystem", "Universal Save System", "1.0.0")]
+    public class Mod : BaseUnityPlugin
+    {
+        public static new ManualLogSource Logger { get; private set; }
+
+        private void Awake()
+        {
+            Logger = base.Logger;
+        }
+    }
+}
