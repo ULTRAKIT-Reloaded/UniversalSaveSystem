9581554f63733ef1211fe731d31967fa613ba282
PetersonE1 <PetersonE_1@live.com>
2024-05-20 23:11:48 -0400

    assembly info

--- ULTRAKIT.UniversalSaveSystem/Properties/AssemblyInfo.cs
+++ ULTRAKIT.UniversalSaveSystem/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ULTRAKIT.UniversalSaveSystem")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("BepInEx plugin to store/retrieve persistent data via json in a text file.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP")]
-[assembly: AssemblyProduct("ULTRAKIT.UniversalSaveSystem")]
+[assembly: AssemblyCompany("ULTRAKIT")]
+[assembly: AssemblyProduct("UniversalSaveSystem")]
 [assembly: AssemblyCopyright("Copyright © HP 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
